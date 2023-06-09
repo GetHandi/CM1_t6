01cf292969e025d92e6e4cb0a6b5a45970f7ba29
GetHandi <102155679+GetHandi@users.noreply.github.com>
2023-06-09 23:04:13 +0900

    done

--- t6.cpp
+++ t6.cpp
@@ -2,39 +2,60 @@
 // Решение нелинейных уравнений методом половинного деления;
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-
-
-
-
-int main()
+double fxa(double x)
 {
-    int n = 0;
-    cout << "Iteration #" << n << ;
-    while (b - a >= 2 * eps)
-    {
+    return x + pow(2, x) + 5;
+}
 
-    }
-
-
-
-
-    return 0;
+double fxb(double x)
+{
+    return 3 * pow(x, 3) + pow(x, 2) + 2 * x + 3;
 }
 
 
-n = 0
-print(f'Итерация: {n:3d}, a = {a:.5f}, b = {b:.5f}, c = {(a + b) / 2:.5f}')
-while b - a >= 2 * eps:
-c = (a + b) / 2
-if f(c) == 0 :
-    return c
-    elif f(a) * f(c) < 0 :
-    b = c
-    elif f(b) * f(c) < 0 :
-    a = c
-    n += 1
-    print(f'Итерация: {n:3d}, a = {a:.5f}, b = {b:.5f}, c = {(a + b) / 2:.5f}')
-    return (a + b) / 2
+double a = -6.0; // крайнее левое значение промежутка  (a: -6;-4; b: -1;0)
+double b = -4.0; // крайнее правое значение промежутка;
+//double a = -1.0; // крайнее левое значение промежутка  (a: -6;-4; b: -1;0)
+//double b = -0.0; // крайнее правое значение промежутка;
+double c = 0.0; // ответ;
+const double eps = 0.000001; // отклонение; 
+
+int main()
+{
+    cout << "x + 2^x + 5 = 0" << endl << endl;
+    int n = 0; // счетчик;
+    cout << "Iteration #" << n << ":\t" << "a = " << a << ",\t" << "b = " << b << "\t" << "answer - " << c << "\n";
+    while (b - a >= 2 * eps) {
+        c = (a + b) / 2;
+        if (fxa(c) == 0)
+            return c;
+        else if (fxa(a) * fxa(c) < 0)
+            b = c;
+        else if (fxa(b) * fxa(c) < 0)
+            a = c;
+        n++;
+        cout << "Iteration #" << n << ":\t" << "a = " << a << ",\t" << "b = " << b << "\t" << "answer - " << c << "\n";
+    }
+    return (a + b) / 2;
+
+    /*cout << "3x^3 + x^2 + 2x + 3 = 0" << endl << endl;
+    int n = 0; // счетчик;
+    cout << "Iteration #" << n << ":\t" << "a = " << a << ",\t" << "b = " << b << "\t" << "answer - " << c << "\n";
+    while (b - a >= 2 * eps) {
+        c = (a + b) / 2;
+        if (fxb(c) == 0)
+            return c;
+        else if (fxb(a) * fxb(c) < 0)
+            b = c;
+        else if (fxb(b) * fxb(c) < 0)
+            a = c;
+        n++;
+        cout << "Iteration #" << n << ":\t" << "a = " << a << ",\t" << "b = " << b << "\t" << "answer - " << c << "\n";
+    }
+    return (a + b) / 2;
+    */
+}
